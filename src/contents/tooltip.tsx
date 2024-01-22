bc7b50935c5d562eacb76a41c88b068296bfd89f
Mardon <mardon.masharbov@gmail.com>
2024-01-22 18:27:00 +0400

    Tooltip. showing, even handling

--- src/contents/tooltip.tsx
+++ src/contents/tooltip.tsx
@@ -1,38 +1,40 @@
-export {};
+import React, { type FC } from "react";
+import type {
+  PlasmoCSConfig,
+  PlasmoCSUIJSXContainer,
+  PlasmoCSUIProps,
+  PlasmoRender,
+} from "plasmo";
+import { createRoot } from "react-dom/client";
+
 console.log(
   "You may find that having is not so pleasing a thing as wanting. This is not logical, but it is often true.",
 );
 
-let tooltipElem: HTMLDivElement | null = null;
-document.addEventListener("selectionchange", (event) => {
-  console.log("selectionchange");
-  if (!window?.getSelection()?.toString()) {
-    tooltipElem?.remove();
+const TOOLTIP_WIDTH = 32;
+const TOOLTIP_HEIGHT = 32;
+const TOOLTIP_ID = "projectx-tooltip";
+
+// selectionchange didn't work because when you click on tooltip
+// and hold your click selectionchange was firing and hidingTooltip before it's able to handle the click event
+document.addEventListener("mousedown", (event) => {
+  const isTooltipClicked = getTooltipElem()?.contains(event.target as Node);
+
+  if (!isHighlighting() || !isTooltipClicked) {
+    hideTooltip();
   }
 });
 
-document.addEventListener("mouseup", () => {
-  console.log("mouseup");
-  tooltipElem?.remove();
-
-  const selection = window.getSelection();
-  if (selection?.isCollapsed) return;
-
-  const position = getSelectedTextPosition();
-  if (!position) return;
-  console.log("position", position);
-
-  if (isAnchorBeforeFocus()) {
-    createTooltip({ left: position.right, top: position.bottom });
-  } else {
-    createTooltip({ left: position.left - 64, top: position.top - 24 });
+document.addEventListener("mouseup", (event: MouseEvent) => {
+  console.log("mouseup event", event);
+  if (isHighlighting()) {
+    showTooltip();
   }
 });
 
-function getSelectedTextPosition() {
+function getSelectedTextPosition(selection: Selection) {
   let selectionStartCoords: DOMRect | null = null,
     selectionEndCoords: DOMRect | null = null;
-  const selection = window.getSelection();
   if (selection?.rangeCount && selection.rangeCount > 0) {
     const range = selection.getRangeAt(0).cloneRange();
 
@@ -45,7 +47,7 @@ function getSelectedTextPosition() {
     startRange.setStart(range.startContainer, range.startOffset);
     startRange.collapse(true);
     startRange.insertNode(startSpan);
-    selectionStartCoords = getOffsetRect(startSpan);
+    selectionStartCoords = getOffsetRectRelativeToBody(startSpan);
     const startSpanParent = startSpan.parentNode;
     if (!startSpanParent) return;
     startSpanParent.removeChild(startSpan);
@@ -56,7 +58,7 @@ function getSelectedTextPosition() {
     endRange.setStart(range.endContainer, range.endOffset);
     endRange.collapse(true);
     endRange.insertNode(endSpan);
-    selectionEndCoords = getOffsetRect(endSpan);
+    selectionEndCoords = getOffsetRectRelativeToBody(endSpan);
     const endSpanParent = endSpan.parentNode;
     if (!endSpanParent) return;
     endSpanParent.removeChild(endSpan);
@@ -83,18 +85,7 @@ function getSelectedTextPosition() {
   };
 }
 
-function createTooltip(position: { left: number; top: number }) {
-  tooltipElem = document.createElement("div");
-  tooltipElem.innerText = "highlight";
-  tooltipElem.style.position = "absolute";
-  tooltipElem.style.zIndex = "100000";
-  tooltipElem.style.transform = `translate(${position.left}px, ${position.top}px)`;
-  tooltipElem.style.transformOrigin = "top left";
-  document.body.insertBefore(tooltipElem, document.body.firstChild);
-}
-
-function isAnchorBeforeFocus() {
-  const selection = window.getSelection();
+function isAnchorBeforeFocus(selection: Selection) {
   if (!selection) return;
   let isAnchorBeforeFocus = true;
   if (selection.anchorNode === selection.focusNode) {
@@ -141,8 +132,7 @@ function isAnchorBeforeFocus() {
   return isAnchorBeforeFocus;
 }
 
-// BoundingClientRect relative to the document
-const getOffsetRect = (el: HTMLElement): DOMRect => {
+function getOffsetRectRelativeToBody(el: HTMLElement): DOMRect {
   const rect = el.getBoundingClientRect();
 
   // add window scroll position to get the offset position
@@ -171,4 +161,113 @@ const getOffsetRect = (el: HTMLElement): DOMRect => {
     height,
     toJSON: () => Object,
   };
+}
+
+function isHighlighting() {
+  return window.getSelection && window.getSelection()?.type === "Range";
+}
+
+function getTooltipElem() {
+  return document.getElementById(TOOLTIP_ID);
+}
+
+function showTooltip() {
+  console.log("showTooltip");
+
+  const tooltip = getTooltipElem();
+  if (!tooltip) return;
+
+  const selection = window.getSelection();
+  if (!selection) return;
+
+  const position = getSelectedTextPosition(selection);
+  if (!position) return;
+
+  if (isAnchorBeforeFocus(selection)) {
+    tooltip.style.transform = `translate(${position.right + 4}px, ${
+      position.bottom + 4
+    }px)`;
+  } else {
+    tooltip.style.transform = `translate(${
+      position.left - TOOLTIP_WIDTH - 4
+    }px, ${position.top - TOOLTIP_HEIGHT - 4}px)`;
+  }
+  tooltip.style.display = "block";
+}
+
+function hideTooltip() {
+  const tooltip = getTooltipElem();
+  if (tooltip) tooltip.style.display = "none";
+}
+
+export const config: PlasmoCSConfig = {
+  matches: ["https://*/*"],
 };
+
+export const getRootContainer = () =>
+  new Promise((resolve) => {
+    const checkInterval = setInterval(() => {
+      const rootContainerParent = document.body;
+      if (rootContainerParent) {
+        clearInterval(checkInterval);
+        const rootContainer = document.createElement("div");
+        rootContainerParent.appendChild(rootContainer);
+        resolve(rootContainer);
+      }
+    }, 137);
+  });
+
+const Tooltip: FC<PlasmoCSUIProps> = () => {
+  const handleTooltipClick = () => {
+    console.log("handleTooltipClick event");
+    if (!isHighlighting()) {
+      hideTooltip();
+    }
+  };
+
+  return (
+    <div
+      id={TOOLTIP_ID}
+      style={{
+        width: TOOLTIP_WIDTH,
+        height: TOOLTIP_HEIGHT,
+        borderRadius: "4px",
+        border: "1px solid #333",
+        backgroundColor: "white",
+        cursor: "pointer",
+        position: "absolute",
+        top: 0,
+        left: 0,
+        zIndex: 100000,
+        transformOrigin: "top left",
+        display: "none",
+      }}
+      onClick={handleTooltipClick}
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth="1"
+        stroke="currentColor"
+        className="h-6 w-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3.75 9h16.5m-16.5 6.75h16.5"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export const render: PlasmoRender<PlasmoCSUIJSXContainer> = async ({
+  createRootContainer,
+}) => {
+  const rootContainer = await createRootContainer!();
+  const root = createRoot(rootContainer);
+  root.render(<Tooltip />);
+};
+
+export default Tooltip;
