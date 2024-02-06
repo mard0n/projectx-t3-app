5bfb8fc11cbef182ef5dec35dc42fd445076db3d
Mardon <mardon.masharbov@gmail.com>
2024-02-06 10:57:12 +0400

    Refactored: Sending updates

--- src/plugins/SendingUpdatesPlugin/index.tsx
+++ src/plugins/SendingUpdatesPlugin/index.tsx
@@ -2,33 +2,21 @@ import React, { type FC, useEffect, useRef } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { mergeRegister } from "@lexical/utils";
 import type { NodeMutation } from "lexical";
-import { $getNodeByKey, TextNode, LineBreakNode } from "lexical";
-import { $findParentBlockContainer } from "~/nodes/Block";
+import { $getNodeByKey, TextNode, LineBreakNode, ParagraphNode } from "lexical";
 import { throttle } from "~/utils/lexical";
 import {
   BlockContainerNode,
-  BlockTextNode,
+  BlockContentNode,
   BlockChildContainerNode,
-} from "../HierarchicalBlocksPlugin";
-import {
-  BlockHeaderNode,
-  type SerializedBlockHeaderNode,
-} from "~/nodes/BlockHeader";
-import {
-  BlockParagraphNode,
-  SerializedBlockParagraphNodeSchema,
-  type SerializedBlockParagraphNode,
-} from "~/nodes/BlockParagraph";
+  type SerializedBlockContainerNode,
+} from "~/nodes/Block";
+
 import { z } from "zod";
-import { SerializedBlockHeaderNodeSchema } from "~/nodes/BlockHeader/BlockHeaderNode";
 import {
-  type SerializedBlockHighlightSliceNode,
-  SerializedBlockHighlightSliceNodeSchema,
-} from "~/nodes/BlockHighlightSlice";
-import {
-  type SerializedBlockHighlightParagraphNode,
-  SerializedBlockHighlightParagraphNodeSchema,
-} from "~/nodes/BlockHighlightParagraph";
+  SerializedBlockContainerNodeSchema,
+  $findParentBlockContainer,
+} from "~/nodes/Block";
+import { HeaderNode } from "~/nodes/Header";
 
 export const updatedBlocksSchema: z.ZodSchema<UpdatedBlock> = z.object({
   updateType: z.union([
@@ -37,25 +25,13 @@ export const updatedBlocksSchema: z.ZodSchema<UpdatedBlock> = z.object({
     z.literal("destroyed"),
   ]),
   updatedBlockId: z.string().uuid(),
-  updatedBlock: z
-    .union([
-      SerializedBlockParagraphNodeSchema,
-      SerializedBlockHeaderNodeSchema,
-      SerializedBlockHighlightSliceNodeSchema,
-      SerializedBlockHighlightParagraphNodeSchema,
-    ])
-    .nullable(),
+  updatedBlock: SerializedBlockContainerNodeSchema.nullable(),
 });
 
 export type UpdatedBlock = {
   updateType: NodeMutation;
   updatedBlockId: string;
-  updatedBlock:
-    | SerializedBlockHeaderNode
-    | SerializedBlockParagraphNode
-    | SerializedBlockHighlightSliceNode
-    | SerializedBlockHighlightParagraphNode
-    | null;
+  updatedBlock: SerializedBlockContainerNode | null;
 };
 
 export type Updates = Map<string, UpdatedBlock>;
@@ -82,91 +58,92 @@ const SendingUpdatesPlugin: FC<SendingUpdatesPluginProps> = ({
     if (
       !editor.hasNodes([
         BlockContainerNode,
-        BlockTextNode,
+        BlockContentNode,
         BlockChildContainerNode,
+        ParagraphNode,
+        HeaderNode,
       ])
     ) {
       throw new Error(
-        "HierarchicalBlockPlugin: BlockContainerNode, BlockTextNode, or BlockChildContainerNode not registered on editor",
+        "HierarchicalBlockPlugin: BlockContainerNode, BlockContentNode, BlockChildContainerNode, ParagraphNode, or HeaderNode not registered on editor",
       );
     }
 
     return mergeRegister(
       // To send updates
-      ...[TextNode, LineBreakNode, BlockTextNode, BlockChildContainerNode].map(
-        (Node) =>
-          editor.registerMutationListener(
-            Node,
-            (mutations, { prevEditorState }) => {
-              editor.getEditorState().read(() => {
-                for (const [nodeKey, mutation] of mutations) {
-                  const node = $getNodeByKey(nodeKey);
-                  if (!node || mutation === "destroyed") {
-                    prevEditorState.read(() => {
-                      const prevNode = $getNodeByKey(nodeKey);
-                      if (!prevNode) return;
+      ...[
+        TextNode,
+        LineBreakNode,
+        ParagraphNode,
+        HeaderNode,
+        BlockContentNode,
+        BlockChildContainerNode,
+      ].map((Node) =>
+        editor.registerMutationListener(
+          Node,
+          (mutations, { prevEditorState }) => {
+            editor.getEditorState().read(() => {
+              for (const [nodeKey, mutation] of mutations) {
+                const node = $getNodeByKey(nodeKey);
+                if (!node || mutation === "destroyed") {
+                  prevEditorState.read(() => {
+                    const prevNode = $getNodeByKey(nodeKey);
+                    if (!prevNode) return;
 
-                      const parentContainer =
-                        $findParentBlockContainer(prevNode);
+                    const parentContainer = $findParentBlockContainer(prevNode);
 
-                      const parentKey = parentContainer?.getKey();
+                    const parentKey = parentContainer?.getKey();
 
-                      if (!parentKey) return;
-                      editor.getEditorState().read(() => {
-                        const updatedParentNode =
-                          $getNodeByKey<BlockContainerNode>(parentKey);
+                    if (!parentKey) return;
+                    editor.getEditorState().read(() => {
+                      const updatedParentNode =
+                        $getNodeByKey<BlockContainerNode>(parentKey);
 
-                        if (updatedParentNode) {
-                          updatesRef.current.set(
-                            `${updatedParentNode.getKey()}:updated`,
-                            {
-                              updateType: "updated",
-                              updatedBlockId: updatedParentNode.getId(),
-                              updatedBlock: null,
-                            },
-                          );
-                        }
-                      });
+                      if (updatedParentNode) {
+                        updatesRef.current.set(
+                          `${updatedParentNode.getKey()}:updated`,
+                          {
+                            updateType: "updated",
+                            updatedBlockId: updatedParentNode.getId(),
+                            updatedBlock: null,
+                          },
+                        );
+                      }
                     });
-                    continue;
-                  }
-
-                  const parentContainer = $findParentBlockContainer(node) as
-                    | BlockParagraphNode
-                    | BlockHeaderNode; // TODO: This is tedious way of doing it.
-
-                  if (parentContainer) {
-                    updatesRef.current.set(
-                      `${parentContainer.getKey()}:${mutation}`,
-                      {
-                        updateType: mutation,
-                        updatedBlockId: parentContainer.getId(),
-                        updatedBlock: parentContainer.exportJSON(),
-                      },
-                    );
-                  }
+                  });
+                  continue;
                 }
 
-                throttleUpdate(updatesRef.current, updatesRef);
-              });
-            },
-          ),
+                const parentContainer = $findParentBlockContainer(node);
+
+                if (parentContainer) {
+                  updatesRef.current.set(
+                    `${parentContainer.getKey()}:${mutation}`,
+                    {
+                      updateType: mutation,
+                      updatedBlockId: parentContainer.getId(),
+                      updatedBlock: parentContainer.exportJSON(),
+                    },
+                  );
+                }
+              }
+
+              throttleUpdate(updatesRef.current, updatesRef);
+            });
+          },
+        ),
       ),
-      ...[BlockParagraphNode, BlockHeaderNode].map((node) => {
+      ...[BlockContainerNode].map((node) => {
         return editor.registerMutationListener(
           node,
           (mutations, { prevEditorState }) => {
             editor.getEditorState().read(() => {
               for (const [nodeKey, mutation] of mutations) {
-                const node = $getNodeByKey<
-                  BlockParagraphNode | BlockHeaderNode // TODO: This one as well. Not ideal
-                >(nodeKey);
+                const node = $getNodeByKey<BlockContainerNode>(nodeKey);
 
                 if (!node || mutation === "destroyed") {
                   prevEditorState.read(() => {
-                    const prevNode = $getNodeByKey<
-                      BlockParagraphNode | BlockHeaderNode // TODO: This one as well. Not ideal
-                    >(nodeKey);
+                    const prevNode = $getNodeByKey<BlockContainerNode>(nodeKey);
                     if (!prevNode) return;
 
                     updatesRef.current.set(`${nodeKey}:destroyed`, {
